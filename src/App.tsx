ebb466d43814cf3a47953b37b9c4673443ef1997
Yumin Cho <dbals081216@gmail.com>
2024-03-28 00:52:05 +0900

    :sparkles: add paper title and abs

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,39 @@
-import "./App.css";
-import Demo from "./section/Demo";
 import styled from "@emotion/styled";
 
+import "./App.css";
 import { colors } from "@/styles/color";
+import Demo from "./section/Demo";
+import paperData from "./data/paper";
 
 const Title = styled.h1`
   font-size: 32px;
   font-weight: bold;
-  padding: 10px;
+  padding: 28px;
+  text-wrap: balance;
+`;
+
+const Section = styled.div<{ white?: boolean }>`
+  display: flex;
+  flex-direction: column;
+  background-color: ${(props) => (props.white ? colors.white : colors.blue200)};
+  padding: 24px 32px;
+  align-items: center;
+  gap: 4px;
+`;
+
+const SubTitle = styled.h2`
+  width: 100%;
+  font-size: 24px;
+  font-weight: bold;
+  text-align: left;
+  max-width: 960px;
 `;
 
 const Abstract = styled.div`
-  background-color: ${colors.blue200};
-  padding: 10px;
   font-size: 17px;
+  text-wrap: balance;
+  text-align: left;
+  max-width: 960px;
 `;
 
 const Author = styled.div`
@@ -27,10 +47,16 @@ const Result = styled.div`
 function App() {
   return (
     <>
-      <Title>Some Title</Title>
-      <Abstract>abstract</Abstract>
+      <Title>{paperData.title}</Title>
+      <Section>
+        <SubTitle>Abstract</SubTitle>
+        <Abstract>{paperData.abstract}</Abstract>
+      </Section>
       <Author>author</Author>
-      <Demo />
+      <Section white>
+        <SubTitle>Playground</SubTitle>
+        <Demo />
+      </Section>
       <Result>result</Result>
     </>
   );
