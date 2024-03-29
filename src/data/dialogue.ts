ed7bba0c718c315c0e8e408a3e2b67c395298d76
Yumin Cho <dbals081216@gmail.com>
2024-03-29 20:37:04 +0900

    :memo: add docstring to dialogue.ts

--- src/data/dialogue.ts
+++ src/data/dialogue.ts
@@ -5,6 +5,14 @@ import realDialogue4 from "./realDialogue/qwdlkqjwd_8.json";
 import realDialogue5 from "./realDialogue/qwdlkqjwd_4.json";
 import realDialogue6 from "./realDialogue/lwkejflkwjefwljefj_1.json";
 
+/**
+ * Aggregated dialogue data for each JSON file.
+ * When adding new data, you must customize the key.
+ * This key will be used as the tab name in the UI.
+ * Before adding new data, make sure to add the JSON file to the `realDialogue` folder.
+ * And you MUST add the following format of object to the top of JSON file:
+ * @example   {"userID": string, "shortcutID": string, "shortcutIndex": number, "exampleRequest": string[]}
+ */
 const dialogueData = {
   "Writing comments": realDialogue1,
   "Vegetarian and gluten-free recipe": realDialogue2,
